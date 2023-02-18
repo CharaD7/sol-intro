906973df406d3a63570a26418a07dd2ac51de50e
CharaD7 <jijakahn6@gmail.com>
2023-02-18 11:57:04 +0000

    Added function to update message and emit updated signal

--- ThiContract.sol
+++ ThiContract.sol
@@ -5,4 +5,9 @@ contract ThiContract {
   string public message = 'Hello World';
 
   event MessageUpdated(address indexed _user, string _message);
+
+  function updateMessage(string memory, _message) public {
+    message = _message;
+    emit MessageUpdated(msg.sender, _message);
+  }
 }
