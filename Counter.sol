4e33fe340569d02451e9375c0f9053fe61ceda0d
CharaD7 <jijakahn6@gmail.com>
2023-02-18 12:51:24 +0000

    Initialized variables

---
+++ Counter.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+contract Counter {
+  // Declare types and scope for the variables to be used
+  string public name;
+  uint public count;
+
+  // Store a numerical value
+  constructor(string memory _name, uint _initialCount) {
+    name = _name;
+    count = _initialCount;
+  }
+
+  // Increase the count
+
+  // Decrease the count
+
+  // Store / set a name
+
+  // CRUD
+}
